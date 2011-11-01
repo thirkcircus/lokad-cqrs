038928888f81756182ff0f5ac39049be6c6134b0
Alexandr Zaozerskiy <zaozerskyau@gmail.com>
2011-11-02 01:53:35 +0600

    Added views.

---
+++ Framework/Snippets/HttpEndpoint/View/HeatMapView.cs
@@ -0,0 +1,19 @@
+﻿#region (c) 2010-2011 Lokad CQRS - New BSD License 
+// Copyright (c) Lokad SAS 2010-2011 (http://www.lokad.com)
+// This code is released as Open Source under the terms of the New BSD Licence
+// Homepage: http://lokad.github.com/lokad-cqrs/
+#endregion
+
+using System.Drawing;
+using System.Runtime.Serialization;
+using Lokad.Cqrs;
+
+namespace Snippets.HttpEndpoint.View
+{
+    [DataContract]
+    public class HeatMapView : Define.AtomicSingleton
+    {
+        [DataMember]
+        public Bitmap Heatmap { get; set; }
+    }
+}
---
+++ Framework/Snippets/HttpEndpoint/View/PointsView.cs
@@ -0,0 +1,24 @@
+﻿#region (c) 2010-2011 Lokad CQRS - New BSD License 
+// Copyright (c) Lokad SAS 2010-2011 (http://www.lokad.com)
+// This code is released as Open Source under the terms of the New BSD Licence
+// Homepage: http://lokad.github.com/lokad-cqrs/
+#endregion
+
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Lokad.Cqrs;
+
+namespace Snippets.HttpEndpoint.View
+{
+    [DataContract]
+    public class PointsView : Define.AtomicSingleton
+    {
+        public PointsView()
+        {
+            Points = new List<HeatPoint>();
+        }
+
+        [DataMember]
+        public List<HeatPoint> Points { get; set; }
+    }
+}
