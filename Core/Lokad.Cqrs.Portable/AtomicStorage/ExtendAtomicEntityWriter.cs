5e4a4dfb23257a653ba3e5cf3b4ad9e46adc05a5
Rinat Abdullin <rinat.abdullin@gmail.com>
2012-01-10 12:38:30 +0100

    Add comments to IAtomicEntityWriter extensions

--- Core/Lokad.Cqrs.Portable/AtomicStorage/ExtendAtomicEntityWriter.cs
+++ Core/Lokad.Cqrs.Portable/AtomicStorage/ExtendAtomicEntityWriter.cs
@@ -4,6 +4,17 @@ namespace Lokad.Cqrs.AtomicStorage
 {
     public static class ExtendAtomicEntityWriter
     {
+        /// <summary>
+        /// Given a <paramref name="key"/> either adds a new <typeparamref name="TEntity"/> OR updates an existing one.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="self">The self.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="addFactory">The add factory (used to create a new entity, if it is not found).</param>
+        /// <param name="update">The update method (called to update an existing entity, if it exists).</param>
+        /// <param name="hint">The hint.</param>
+        /// <returns></returns>
         public static TEntity AddOrUpdate<TKey,TEntity>(this IAtomicWriter<TKey, TEntity> self, TKey key, Func<TEntity> addFactory, Action<TEntity> update, AddOrUpdateHint hint = AddOrUpdateHint.ProbablyExists)
         {
             return self.AddOrUpdate(key, addFactory, entity =>
@@ -12,6 +23,17 @@ namespace Lokad.Cqrs.AtomicStorage
                     return entity;
                 }, hint);
         }
+        /// <summary>
+        /// Given a <paramref name="key"/> either adds a new <typeparamref name="TEntity"/> OR updates an existing one.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="self">The self.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="newView">The new view that will be saved, if entity does not already exist</param>
+        /// <param name="updateViewFactory">The update method (called to update an existing entity, if it exists).</param>
+        /// <param name="hint">The hint.</param>
+        /// <returns></returns>
         public static TEntity AddOrUpdate<TKey,TEntity>(this IAtomicWriter<TKey,TEntity> self, TKey key, TEntity newView, Action<TEntity> updateViewFactory, AddOrUpdateHint hint = AddOrUpdateHint.ProbablyExists)
         {
             return self.AddOrUpdate(key, () => newView, view =>
@@ -21,6 +43,16 @@ namespace Lokad.Cqrs.AtomicStorage
                 }, hint); 
         }
 
+        /// <summary>
+        /// Saves new entity, using the provided <param name="key"></param> and throws 
+        /// <exception cref="InvalidOperationException"></exception> if the entity actually already exists
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="self">The self.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="newEntity">The new entity.</param>
+        /// <returns></returns>
         public static TEntity Add<TKey,TEntity>(this IAtomicWriter<TKey, TEntity> self, TKey key, TEntity newEntity)
         {
             return self.AddOrUpdate(key, newEntity, e =>
@@ -31,6 +63,15 @@ namespace Lokad.Cqrs.AtomicStorage
         }
 
 
+        /// <summary>
+        /// Updates already existing entity, throwing exception, if it does not already exist.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="self">The self.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="change">The change.</param>
+        /// <returns></returns>
         public static TEntity UpdateOrThrow<TKey,TEntity>(this IAtomicWriter<TKey,TEntity> self, TKey key, Func<TEntity, TEntity> change)
         {
             return self.AddOrUpdate(key, () =>
@@ -39,6 +80,15 @@ namespace Lokad.Cqrs.AtomicStorage
                     throw new InvalidOperationException(txt);
                 }, change, AddOrUpdateHint.ProbablyExists);
         }
+        /// <summary>
+        /// Updates already existing entity, throwing exception, if it does not already exist.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="self">The self.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="change">The change.</param>
+        /// <returns></returns>
         public static TEntity UpdateOrThrow<TKey,TEntity>(this IAtomicWriter<TKey,TEntity> self, TKey key, Action<TEntity> change)
         {
             return self.AddOrUpdate(key, () =>
@@ -48,6 +98,16 @@ namespace Lokad.Cqrs.AtomicStorage
                 }, change, AddOrUpdateHint.ProbablyExists);
         }
 
+        /// <summary>
+        /// Updates an entity, creating a new instance before that, if needed.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TView">The type of the view.</typeparam>
+        /// <param name="self">The self.</param>
+        /// <param name="key">The key.</param>
+        /// <param name="update">The update.</param>
+        /// <param name="hint">The hint.</param>
+        /// <returns></returns>
         public static TView UpdateEnforcingNew<TKey,TView>(this IAtomicWriter<TKey,TView> self, TKey key,
             Action<TView> update, AddOrUpdateHint hint = AddOrUpdateHint.ProbablyExists)
             where TView : new()
