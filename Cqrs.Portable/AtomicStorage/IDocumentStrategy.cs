2ec93b513a1fcce54c49dfc9ecb19bb126cb34e2
Rinat Abdullin <rinat.abdullin@gmail.com>
2012-07-05 21:50:23 +0600

    Merge strategies back and add comment

--- Cqrs.Portable/AtomicStorage/IDocumentStrategy.cs
+++ Cqrs.Portable/AtomicStorage/IDocumentStrategy.cs
@@ -10,25 +10,20 @@ using System.IO;
 
 namespace Lokad.Cqrs.AtomicStorage
 {
-    public interface IDocumentStrategy : 
-        IDocumentLocationStrategy,
-        IDocumentSerializationStrategy
-    {
-        
 
-
-        
-    }
-
-    public interface IDocumentSerializationStrategy
+    /// <summary>
+    /// Logically this strategy contains two different aspects: serialization and location.
+    /// However it is more convenient to keep them in one interface, since they are frequently
+    /// passed together (e.g.: in projection management code)
+    /// </summary>
+    public interface IDocumentStrategy 
     {
         void Serialize<TEntity>(TEntity entity, Stream stream);
         TEntity Deserialize<TEntity>(Stream stream);
-    }
 
-    public interface IDocumentLocationStrategy
-    {
         string GetEntityBucket<TEntity>();
         string GetEntityLocation(Type entity, object key);
     }
+
+    
 }
